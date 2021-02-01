ac78086de00d54e6ff9de1593410710698888f2e
RetNerd <raczmarton292@gmail.com>
2021-02-02 00:56:02 +0100

    Spike: Yarn package manager, es6 react, electron-builder packaging

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+const electron = window.require("electron");
+
+function App() {
+  
+  return (
+    <div className="App">
+      <span>That's gonna be a dnd layer</span>
+    </div>
+  );
+}
+
+
+export default App;
